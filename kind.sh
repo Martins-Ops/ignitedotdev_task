0f379f9af55ebf167b988965c24979311860b19e
oluwafemi <oluwafemimartins1234@gmail.com>
2024-02-13 11:20:42 +0100

    fix: change port number

--- kind.sh
+++ kind.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Cluster name 
-CLUSTER_NAME="cluster"
-
-# Directory to store the kubeconfig file
-KUBECONFIG_DIR="$HOME/.kube"
+CLUSTER_NAME="mycluster"
 
 # Create a Kind cluster
 kind create cluster --name $CLUSTER_NAME
+
+# Directory to store the kubeconfig file (Directory already exist)
+KUBECONFIG_DIR="$HOME/.kube"
 
 # Set the kubeconfig context to the new cluster
 kubectl cluster-info --context kind-$CLUSTER_NAME
@@ -15,6 +15,5 @@ kubectl cluster-info --context kind-$CLUSTER_NAME
 # Verify the cluster is running
 kubectl get nodes
 
-# Copy the kubeconfig to a specified Directory
-kubectl config view --minify --flatten --context="kind-$CLUSTER_NAME" > $KUBECONFIG_DIR/config-kind
-
+# Saving the kubeconfig for later use
+kind get kubeconfig --name $CLUSTER_NAME > $KUBECONFIG_DIR/kind-config-mycluster
