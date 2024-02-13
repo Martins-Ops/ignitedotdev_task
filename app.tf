8fd9961d9351c3a206f640234821c5aacf9d56fe
oluwafemi <oluwafemimartins1234@gmail.com>
2024-02-13 14:30:49 +0100

    add terraform script for deployment

--- app.tf
+++ app.tf
@@ -8,27 +8,40 @@ terraform {
   }
 }
 provider "kubectl" {
-  config_path = "~/.kube/config-kind"
+  config_path = "~/.kube/kind-config-mycluster"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config-kind"
+    config_path = "~/.kube/kind-config-mycluster"
   }
 }
 
-data "template_file" "app_yaml" {
-  template = file("${path.module}/app-dep.yaml")  
-}
+# data "template_file" "app_yaml" {
+#   template = file("${path.module}/app-dep.yaml")  
+# }
 
 #Provision the deployment manifest
 resource "kubectl_manifest" "my_app" {
-  yaml_body = data.template_file.app_yaml.rendered
+  yaml_body = file("${path.module}/app-dep.yaml")
 }
 
 #Provisions kube-prometheus stack
-resource "helm_release" "ingress" {
-  name       = "kube-prometheus"
+resource "helm_release" "kube_prom" {
+  name       = "kube-prometheus-stack"
   chart      = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
 }
+
+# ServiceMonitor resources for scraping metrics
+resource "kubectl_manifest" "service_monitor" {
+  yaml_body = file("${path.module}/servicemonitor.yaml")
+  depends_on = [
+    kubectl_manifest.my_app
+  ]
+}
+
